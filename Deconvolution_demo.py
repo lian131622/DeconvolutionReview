c8ec19ff5f9787976272984bfb666eb644397f68
lian4 <lian_john@163.com>
2025-07-25 11:06:15 +0200

    modify deconvolution.py

--- Deconvolution_demo.py
+++ Deconvolution_demo.py
@@ -1,25 +1,23 @@
 import numpy as np
-from scipy.linalg import toeplitz
 from scipy.signal import find_peaks
-from spectrum import arburg
+from scipy.linalg import toeplitz
 
-
-def OMP(Signal, Reference, N):
+def OMP(Signal, X, N):
     '''
 
     Parameters
     ----------
     Signal : Measured signal.
     
-    Reference : Reference signal.
+    X : Dictionary.
     
     N : Number of iterations.
 
     Returns
     -------
-    amp : Vector of amplitudes
+    amp : Vector of amplitudes.
     
-    taus : Vector of ToFs
+    taus : Vector of ToFs.
     
     SignalR : Estimated impulse response.
 
@@ -27,14 +25,9 @@ def OMP(Signal, Reference, N):
 
     # Initialization
     Ns = len(Signal)
-    Nw = len(Reference)
     res = Signal
     taus = []
-
-    # Create dictionary
-    X = toeplitz(np.r_[Reference, np.zeros(Ns - Nw)],
-                 np.r_[Reference[0], np.zeros(Ns - 1)])
-
+    
     # Iteration loop
     for _ in range(0, N):
         taus.append(np.argmax(np.abs(res @ X)))
@@ -75,11 +68,9 @@ def SD(Signal, Reference, theta, lam, N_iter=1000, eps=1e-6):
 
     # Initialization
     Ns = len(Signal)
+    Ns = len(Signal)
     Nw = len(Reference)
-
-    # Create Toeplitz matrix
-    X = toeplitz(np.r_[Reference, np.zeros(Ns - Nw)],
-                 np.r_[Reference[0], np.zeros(Ns - 1)])
+    X = toeplitz(np.r_[Reference, np.zeros(Ns-Nw)],np.r_[Reference[0], np.zeros(Ns-1)])
 
     # ISTA algorithm
     hk = np.zeros(Ns)
@@ -170,6 +161,87 @@ def MUSIC(Signal, Reference, NSub, BW=0.1):
 
     return amp, taus, SignalR
 
+
+def arburg(X, order):
+    '''
+    The program utilizes this function adapted from the library spectrum.
+    Cokelaer et al, (2017), 'Spectrum': Spectral Analysis in Python, Journal of Open Source Software, 2(18), 348, doi:10.21105/joss.00348
+    
+    Parameters
+    ----------
+    X : Array of complex data samples (length N)
+    
+    order : Order of autoregressive process (0 < order < N)
+    
+
+    Returns
+    -------
+    a : Array of complex autoregressive parameters A(1) to A(order).
+    
+    rho : Real variable representing driving noise variance (mean square
+      of residual noise) from the whitening operation of the Burg
+      filter.
+      
+    ref : Reflection coefficients defining the filter of the model.
+
+    '''
+    
+    x = np.array(X)
+    N = len(x)
+
+    # Initialisation
+    rho = sum(abs(x)**2.) / float(N)
+    den = rho * 2. * N
+
+
+    a = np.zeros(0, dtype=complex)
+    ref = np.zeros(0, dtype=complex)
+    ef = x.astype(complex)
+    eb = x.astype(complex)
+    temp = 1.
+    
+    #   Main recursion
+    for k in range(0, order):
+
+        # calculate the next order reflection coefficient Eq 8.14 Marple
+        num = sum([ef[j]*eb[j-1].conjugate() for j in range(k+1, N)])
+        den = temp * den - abs(ef[k])**2 - abs(eb[N-1])**2
+        kp = -2. * num / den #eq 8.14
+
+        temp = 1. - abs(kp)**2.
+        new_rho = temp * rho
+
+        # this should be after the criteria
+        rho = new_rho
+
+        a.resize(a.size+1,refcheck=False)
+        a[k] = kp
+        if k == 0:
+            for j in range(N-1, k, -1):
+                save2 = ef[j]
+                ef[j] = save2 + kp * eb[j-1]
+                eb[j] = eb[j-1] + kp.conjugate()*save2
+
+        else:
+            # update the AR coeff
+            khalf = (k+1)//2
+            for j in range(0, khalf):
+                ap = a[j] # previous value
+                a[j] = ap + kp * a[k-j-1].conjugate()
+                if j != k-j-1:
+                    a[k-j-1] = a[k-j-1] + kp * ap.conjugate()
+
+            # update the prediction error
+            for j in range(N-1, k, -1):
+                save2 = ef[j]
+                ef[j] = save2 + kp * eb[j-1]
+                eb[j] = eb[j-1] + kp.conjugate()*save2
+
+        # save the reflection coefficient
+        ref.resize(ref.size+1, refcheck=False)
+        ref[k] = kp
+
+    return a, rho, ref
 
 def AR(Signal, Reference, p, BW=0.1):
     '''
